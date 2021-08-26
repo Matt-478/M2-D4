072d1b94867bb9133fc53eb6f03e06b2165c7aea
Matīss Komļevs <matisskomlevs@gmail.com>
2021-08-26 17:15:19 +0300

    First attempt at task 1

---
+++ function.js
@@ -0,0 +1,10 @@
+// - The user should be able to add a list of names, find a way to get the user input and fill a list with the new name.
+const textInput = document.querySelector(".text-input")
+const ulElementNode = document.querySelector(".ul-element")
+const liElementNode = document.getElementsByTagName("li")
+
+console.log(liElementNode)
+for(i=0; i<liElementNode.length; i++) {
+  console.log(liElementNode[i])
+}
+console.log(liElementNode)
